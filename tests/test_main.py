15011a4e2ac33de3eda579fea26ab2f1474b948b
Marcus Turnbo <marcus@emarcus.com>
2026-04-03 12:47:00 -0500

    Fixed test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,14 @@
-import pytest
 from main import main
 from classes.linked_list import LinkedList
 from classes.singly_linked_list import SinglyLinkedList
 from classes.doubly_linked_list import DoublyLinkedList
 
 
-def test_main_raises_without_values_or_ops_file(monkeypatch):
+def test_main_raises_without_values_or_ops_file(monkeypatch, capsys):
     monkeypatch.setattr("sys.argv", ["main.py", "singly", "print"])
-    with pytest.raises(ValueError, match="Must specify either values or operations file"):
-        main()
+    main()
+    captured = capsys.readouterr()
+    assert "Must specify either values or operations file" in captured.out
 
 def test_doubly_lltype_creates_singly_linked_list():
     ll = LinkedList.create("singly")
@@ -17,4 +17,3 @@ def test_doubly_lltype_creates_singly_linked_list():
 def test_doubly_lltype_creates_doubly_linked_list():
     ll = LinkedList.create("doubly")
     assert isinstance(ll, DoublyLinkedList)
-
